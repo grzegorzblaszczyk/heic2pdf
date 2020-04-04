79a26a92a630a0befd1b63cb59f7c0839de3c4e1
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2020-04-05 00:55:23 +0200

    UPGRADE: v1.1 - using shell-proteins-ext with function heic2pdf

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # heic2pdf
 HEIC (iPhone images) to PDF converter using ImageMagick
+
+## Usage
+
+```
+./heic2pdf.sh IMG_0001.HEIC output.pdf
+```
+
+This will convert iPhone image to a PDF file with colorspace sRGB and resampled as 100 ppi.
--- heic2pdf.sh
+++ heic2pdf.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-# heic2pdf v1.0
+# heic2pdf v1.1
 
-if [ "x$1" == "x" ] || [ "x$2" == "x" ]; then 
-  echo "Usage:"
-  echo "$0 [input_file] [outputfile]"
-  exit 1;
+CURL=`which curl`
+if [ ! -d "$HOME/bin/shell-proteins" ]; then echo "Setup of shell-proteins..." && ${CURL} -sSL https://raw.githubusercontent.com/grzegorzblaszczyk/shell-proteins/master/setup.sh | bash; fi
+
+# This script:
+# * converts HEIC file (iPhone image) to JPG
+# * converts JPG file to PDF
+
+source "$HOME/bin/shell-proteins/bash/base.inc.sh"
+protein_require "core/print_usage"
+protein_require "grzegorzblaszczyk/shell-proteins-ext/bash/os_agnostic/verify_if_installed_with_dot" "https://raw.githubusercontent.com/grzegorzblaszczyk/shell-proteins-ext/master/bash/os_agnostic/verify_if_installed_with_dot.inc.sh"
+protein_require "grzegorzblaszczyk/shell-proteins-ext/bash/convert/heic2pdf" "https://raw.githubusercontent.com/grzegorzblaszczyk/shell-proteins-ext/master/bash/convert/heic2pdf.inc.sh"
+
+if [ "x${1}" == "x" ] || [ "x${2}" == "x" ]; then
+  print_usage "HEIC input file" "PDF output file"
+  exit 1
 fi
 
-CONVERT=`which convert`
-RM=`which rm`
-INPUT_FILE="$1"
-OUTPUT_FILE="$2"
-TEMP_FILE="${INPUT_FILE%.*}.jpg"
+verify_if_installed_with_dot "convert" "/usr/local/bin/convert"
+heic2pdf ${1} ${2}
 
-if [ "x${CONVERT}" == "x" ]; then
-  echo "Please install ImageMagick first!"
-  exit 1;
-fi
-
-COLORSPACE="sRGB"
-UNITS="PixelsPerInch"
-RESAMPLE_VALUE="100"
-
-echo "Converting ${INPUT_FILE} to ${OUTPUT_FILE} using ${TEMP_FILE}.."
-
-${CONVERT} "${INPUT_FILE}" -colorspace "${COLORSPACE}" -units "${UNITS}" -resample "${RESAMPLE_VALUE}" "${TEMP_FILE}" && ${CONVERT} "${TEMP_FILE}" "${OUTPUT_FILE}" && ${RM} "${TEMP_FILE}"
